140ed113eea74c7e3f32f823c857dfc45168d4f0
ayaouakka-hub <aya.ouakka@edu.umi.ac.ma>
2026-04-20 23:53:08 +0200

    Ajout data des barrages

--- AVR_DATA/test_gee.py
+++ AVR_DATA/test_gee.py
@@ -6,7 +6,6 @@ try:
     ee.Initialize(project=MY_PROJECT_ID)
     print("NADI ! GEE m-connecter w m9ad.")
     
-    # Test simple : Njbdou l'altitude d Meknès par exemple
     dem = ee.Image('USGS/SRTMGL1_003')
     point = ee.Geometry.Point([-5.55, 33.89]) # Meknès
     val = dem.sample(point, 30).first().get('elevation').getInfo()
